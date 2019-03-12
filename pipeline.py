c63b7cfbb354e02bfc0403bcd94ac8ac0cbcb5a0
Tobias Horsmann <tobias.horsmann@gmail.com>
2019-03-12 19:07:00 +0100

    adjusted order of parameters

--- pipeline.py
+++ pipeline.py
@@ -2,32 +2,32 @@ from dkpro_core_pypeline.DKProCorePyPeline import *
 from os.path import expanduser
 user_home = expanduser("~")
 
-reader = CollectionReader(component = "de.tudarmstadt.ukp.dkpro.core.io.text.StringReader", 
-					          group = "de.tudarmstadt.ukp.dkpro.core",
-					       artifact = "de.tudarmstadt.ukp.dkpro.core.io.text-asl",
-					       language = "en", 
-					        version = "1.10.0",
-					    document_id = "123",
-				      document_text = "This is a text. Isn't this cool?")
-segmenter = AnalysisEngine(component = "de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter",
-					           group = "de.tudarmstadt.ukp.dkpro.core",
-					         version = "1.10.0",
-					        artifact = "de.tudarmstadt.ukp.dkpro.core.tokit-asl")
-pos = AnalysisEngine(component = "de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpPosTagger", 
-				         group = "de.tudarmstadt.ukp.dkpro.core",
-				       version = "1.10.0",
-				      artifact = "de.tudarmstadt.ukp.dkpro.core.opennlp-asl",
-				       variant = "maxent")
-writer = AnalysisEngine(component = "de.tudarmstadt.ukp.dkpro.core.io.conll.Conll2003Writer", 
-				            group = "de.tudarmstadt.ukp.dkpro.core",
-				          version = "1.9.0",
-				         artifact = "de.tudarmstadt.ukp.dkpro.core.io.conll-asl",
-				        overwrite = "true",
-				      write_chunk = "false",
-			   write_named_entity = "false",
-				  target_location = user_home + "/Desktop/dkpro_output_folder",
-				  target_encoding = "utf-8",
-			   filename_extension = ".txt")
+reader = CollectionReader(group = "de.tudarmstadt.ukp.dkpro.core",
+					   artifact = "de.tudarmstadt.ukp.dkpro.core.io.text-asl",
+				      component = "de.tudarmstadt.ukp.dkpro.core.io.text.StringReader", 
+					   language = "en", 
+					    version = "1.10.0",
+					document_id = "123",
+				  document_text = "This is a text. Isn't this cool?")
+segmenter = AnalysisEngine(group = "de.tudarmstadt.ukp.dkpro.core",
+                        artifact = "de.tudarmstadt.ukp.dkpro.core.tokit-asl",
+					   component = "de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter",
+					     version = "1.10.0")
+pos = AnalysisEngine(group = "de.tudarmstadt.ukp.dkpro.core",
+				  artifact = "de.tudarmstadt.ukp.dkpro.core.opennlp-asl",
+				 component = "de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpPosTagger", 
+				   version = "1.10.0",
+				   variant = "maxent")
+writer = AnalysisEngine(group = "de.tudarmstadt.ukp.dkpro.core",
+				     artifact = "de.tudarmstadt.ukp.dkpro.core.io.conll-asl",
+				    component = "de.tudarmstadt.ukp.dkpro.core.io.conll.Conll2003Writer", 
+				      version = "1.9.0",
+                    overwrite = "true",
+				  write_chunk = "false",
+		   write_named_entity = "false",
+			  target_location = user_home + "/Desktop/dkpro_output_folder",
+			  target_encoding = "utf-8",
+		   filename_extension = ".txt")
 
 pipeline = UIMAPipeline()
 pipeline.set_reader(reader)
